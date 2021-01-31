eea1b6845f9133d5b982a604f6c019e7e4ff3e0f
Chiranjibi karki <ckarki70@gmail.com>
2021-01-31 12:06:00 +0545

    project initialized with dependencies installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+    </Router>
   );
 }
 
