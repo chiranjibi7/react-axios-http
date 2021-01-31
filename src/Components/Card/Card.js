f940f2c1dc829f17b5b58553de932335cbb861d4
Chiranjibi karki <ckarki70@gmail.com>
2021-01-31 18:49:08 +0545

    Button functionality for dynamic routing added

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Card.css';
+import {Link} from 'react-router-dom';
 import {
-    Card, CardImg, CardText, CardBody,
+    Card, CardText, CardBody,
     CardTitle, CardSubtitle, Button
   } from 'reactstrap';
 
-const CardComponent = ({name,username,phone}) => {
+const CardComponent = ({name,username,phone,id}) => {
     return (
         <div className="Card">
       <Card id="card">
@@ -13,7 +14,7 @@ const CardComponent = ({name,username,phone}) => {
           <CardTitle tag="h5"><i className="fas fa-user icon"></i>{name}</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted subtitle">@{username}</CardSubtitle>
           <CardText><i className="fas fa-phone-alt icon"></i>{phone}</CardText>
-          <Button color="info" id="btn">Details</Button>
+          <Link to={`/users/${id}`}><Button color="info" id="btn">Details</Button></Link>
         </CardBody>
       </Card>
     </div>
