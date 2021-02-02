7905e02a34b8672eece7d4bef342f198edc1d083
Chiranjibi karki <ckarki70@gmail.com>
2021-02-02 09:16:25 +0545

    App component finalised after routing added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {Home, Users, User, Post} from './Pages';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/users" exact component={Users}/>
+          <Route path="/users/:id" component={User}/>
+          <Route path="/posts/:id" component={Post}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
