3293e680fa81f5e3fec05df1e45f364b8cb9171c
Chiranjibi karki <ckarki70@gmail.com>
2021-02-02 17:33:53 +0545

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 
 function App() {
   return (
-    <Router>
-    </Router>
+   <div>
+
+   </div>
   );
 }
 
