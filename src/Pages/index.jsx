94b193f83ddbe60a58907c63d0af55ea97a2bed5
Chiranjibi karki <ckarki70@gmail.com>
2021-02-02 09:14:05 +0545

    import export files centralized

---
+++ src/Pages/index.jsx
@@ -0,0 +1,6 @@
+import Home from './Home/Home';
+import Users from './Users/Users';
+import User from '../Components/User/User';
+import Post from '../Components/Post/Post';
+
+export {Home, Users, User, Post};
